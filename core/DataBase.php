3e968af24422eb6fe52a68857cb97bfbf0289d4b
nikitakuznecov <nk_pvc@mail.ru>
2021-08-12 12:12:43 +0400

    Update

--- core/DataBase.php
+++ core/DataBase.php
@@ -1,103 +1,112 @@
 <?
+
 namespace FORM\Core;
 
-class DataBase extends Singleton {
+class DataBase extends Singleton
+{
+    /**
+     * Идентификатор соединения
+     */
+    private $mysqli;
 
-  /**
-   * Идентификатор соединения
-   */
-  private $mysqli;
+    /**
+     * Символ значения в запросе
+     */
+    private $sym_query = "{?}";
 
-  /**
-   * Символ значения в запросе
-   */
-  private $sym_query = "{?}";
+    /**
+     * Поскольку конструктор Одиночки вызывается только один раз, постоянно
+     * открыто всего лишь одино соединение с базой данных.
+     */
+    protected function __construct()
+    {
 
-  /**
-    * Поскольку конструктор Одиночки вызывается только один раз, постоянно
-    * открыто всего лишь одино соединение с базой данных.
-  */
-  protected function __construct() {
+        $config = Config::getInstance();
+        $this->mysqli = new \MySQLi($config->getValue("hostname"), $config->getValue("username"), $config->getValue("password"), $config->getValue("database"));
+        $this->mysqli->query("SET lc_time_names = 'ru_RU'");
+        $this->mysqli->query("SET NAMES 'utf8'");
 
-    $config = Config::getInstance();
-    $this->mysqli = new \MySQLi($config->getValue("hostname"), $config->getValue("username"), $config->getValue("password"), $config->getValue("database"));
-    $this->mysqli->query("SET lc_time_names = 'ru_RU'");
-    $this->mysqli->query("SET NAMES 'utf8'");
-
-  }
-
-  /**
-    * Вспомогательный метод, который заменяет "символ значения в запросе" на конкретное значение, которое проходит через "функции безопасности"
-  */
-  protected function getQuery($query, $params) {
-    if ($params) {
-      for ($i = 0; $i < count($params); $i++) {
-        $pos = strpos($query, $this->sym_query);
-        $arg = "'".$this->mysqli->real_escape_string($params[$i])."'";
-        $query = substr_replace($query, $arg, $pos, strlen($this->sym_query));
     }
+
+    /**
+     * Вспомогательный метод, который заменяет "символ значения в запросе" на конкретное значение, которое проходит через "функции безопасности"
+     */
+    protected function getQuery($query, $params)
+    {
+        if ($params) {
+            for ($i = 0; $i < count($params); $i++) {
+                $pos = strpos($query, $this->sym_query);
+                $arg = "'" . $this->mysqli->real_escape_string($params[$i]) . "'";
+                $query = substr_replace($query, $arg, $pos, strlen($this->sym_query));
+            }
+        }
+        return $query;
     }
-    return $query;
-  }
 
-  /**
-    * SELECT-метод, возвращающий таблицу результатов
-  */
-  public function select($query, $params = false) {
-    $result_set = $this->mysqli->query($this->getQuery($query, $params));
-    if (!$result_set) return false;
-    return $this->resultSetToArray($result_set);
-  }
-
-  /**
-    * SELECT-метод, возвращающий одну строку с результатом
-  */
-  public function selectRow($query, $params = false) {
-    $result_set = $this->mysqli->query($this->getQuery($query, $params));
-    if ($result_set->num_rows != 1) return false;
-    else return $result_set->fetch_assoc();
-  }
-
-  /**
-    * SELECT-метод, возвращающий значение из конкретной ячейки
-  */
-  public function selectCell($query, $params = false) {
-    $result_set = $this->mysqli->query($this->getQuery($query, $params));
-    if ((!$result_set) || ($result_set->num_rows != 1)) return false;
-    else {
-      $arr = array_values($result_set->fetch_assoc());
-      return $arr[0];
+    /**
+     * SELECT-метод, возвращающий таблицу результатов
+     */
+    public function select($query, $params = false)
+    {
+        $result_set = $this->mysqli->query($this->getQuery($query, $params));
+        if (!$result_set) return false;
+        return $this->resultSetToArray($result_set);
     }
-  }
 
-  /**
-    * НЕ-SELECT методы (INSERT, UPDATE, DELETE). Если запрос INSERT, то возвращается id последней вставленной записи
-  */
-  public function query($query, $params = false) {
-    $success = $this->mysqli->query($this->getQuery($query, $params));
-    if ($success) {
-      if ($this->mysqli->insert_id === 0) return true;
-      else return $this->mysqli->insert_id;
+    /**
+     * SELECT-метод, возвращающий одну строку с результатом
+     */
+    public function selectRow($query, $params = false)
+    {
+        $result_set = $this->mysqli->query($this->getQuery($query, $params));
+        if ($result_set->num_rows != 1) return false;
+        else return $result_set->fetch_assoc();
     }
-    else return false;
-  }
-  
-  /**
-   * Преобразование result_set в двумерный массив
-  */
-  protected function resultSetToArray($result_set) {
-    $array = array();
-    while (($row = $result_set->fetch_assoc()) != false) {
-      $array[] = $row;
-    }
-    return $array;
-  }
 
-  /**
-   * При уничтожении объекта закрывается соединение с базой данных
-  */
-  public function __destruct() {
-    if ($this->mysqli) $this->mysqli->close();
-  }
+    /**
+     * SELECT-метод, возвращающий значение из конкретной ячейки
+     */
+    public function selectCell($query, $params = false)
+    {
+        $result_set = $this->mysqli->query($this->getQuery($query, $params));
+        if ((!$result_set) || ($result_set->num_rows != 1)) return false;
+        else {
+            $arr = array_values($result_set->fetch_assoc());
+            return $arr[0];
+        }
+    }
+
+    /**
+     * НЕ-SELECT методы (INSERT, UPDATE, DELETE). Если запрос INSERT, то возвращается id последней вставленной записи
+     */
+    public function query($query, $params = false)
+    {
+        $success = $this->mysqli->query($this->getQuery($query, $params));
+        if ($success) {
+            if ($this->mysqli->insert_id === 0) return true;
+            else return $this->mysqli->insert_id;
+        } else return false;
+    }
+
+    /**
+     * Преобразование result_set в двумерный массив
+     */
+    protected function resultSetToArray($result_set)
+    {
+        $array = array();
+        while (($row = $result_set->fetch_assoc()) != false) {
+            $array[] = $row;
+        }
+        return $array;
+    }
+
+    /**
+     * При уничтожении объекта закрывается соединение с базой данных
+     */
+    public function __destruct()
+    {
+        if ($this->mysqli) $this->mysqli->close();
+    }
 }
+
 ?>
